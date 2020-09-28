d81d6c1abf9297464a0b82341fcb126b90313442
GonzaloRossin <65369181+GonzaloRossin@users.noreply.github.com>
2020-09-28 12:07:28 -0300

    implementación de motor de busqueda por medio de Qgrams

--- src/main/java/ResourceReader.java
+++ src/main/java/ResourceReader.java
@@ -1,6 +1,7 @@
 import model.BusInPath;
 import model.PlaceLocation;
 import utils.Graph;
+import utils.Qgrams;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -9,12 +10,12 @@ import java.io.IOException;
 import java.util.*;
 
 public class ResourceReader {
-    public List<PlaceLocation> getSites(){
-        String Path="C:\\Users\\gonza\\Downloads\\TP EDA - Base\\src\\main\\resources\\espacios-culturales";
+    public static List<PlaceLocation> getSites(){
+
+        String Path="C:\\Users\\gonza\\Downloads\\TP EDA - Base\\src\\main\\resources\\espacios-culturales.csv";
         String line;
         List<PlaceLocation> sites=new ArrayList<>();
         try {
-
             BufferedReader buffer= new BufferedReader(new FileReader(Path));
             buffer.readLine();
             while((line=buffer.readLine())!=null){
@@ -26,10 +27,10 @@ public class ResourceReader {
         }
         return sites;
     }
-    public static void main(String[] args) {
+    public Map<Integer, Map<String, ArrayList<PlaceLocation>>> getBusStops() {
         String Path="G:\\EDA\\EDA-TP\\src\\main\\resources\\paradas-de-colectivo.csv";
         String line;
-        Graph busGraph=new Graph(false);
+
         Map<Integer, Map<String, ArrayList<PlaceLocation>>> holder = new HashMap<>();
         try {
             BufferedReader buffer= new BufferedReader(new FileReader(Path));
@@ -38,13 +39,11 @@ public class ResourceReader {
             while((line=buffer.readLine())!=null){
                 String[] value=line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
                 parseData(holder, Double.parseDouble(value[3]), Double.parseDouble(value[4]), Integer.parseInt(value[7]), value[8], value[10]);
-                //busGraph.addNode();
-                //sites.add(new PlaceLocation(value[3], Double.parseDouble(value[13]), Double.parseDouble(value[14])));
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return ;
+        return holder;
     }
 
     public static void parseData(Map<Integer, Map<String, ArrayList<PlaceLocation>>> holder, Double stop_lat, Double stop_lon, int agency_id, String route_short_name, String route_desc) {
@@ -68,5 +67,28 @@ public class ResourceReader {
                 holder.put(agency_id, routes);
             }
     }
-
+    public static PlaceLocation[] getTop10(String searchterm){
+        int qgrams=8;
+        searchterm=searchterm.toUpperCase();
+        PlaceLocation[] top10=new PlaceLocation[10];
+        boolean flag;
+        Arrays.fill(top10,null);
+        for(PlaceLocation database:getSites()){
+            flag=false;
+            double similarity= Qgrams.similarity(searchterm,database.getName(),qgrams);
+            PlaceLocation candidate=database;
+            for(int i=0;i< top10.length && !flag;i++){
+                if(top10[i]==null){
+                    top10[i]=candidate;
+                    flag=true;
+                }else if(similarity>Qgrams.similarity(searchterm,top10[i].getName(),qgrams)) {
+                    similarity=Qgrams.similarity(searchterm,top10[i].getName(),qgrams);
+                    PlaceLocation aux=top10[i];
+                   top10[i]=candidate;
+                   candidate=aux;
+                }
+            }
+        }
+        return top10;
+    }
 }
